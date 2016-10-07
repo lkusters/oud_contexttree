31f3c4dfd79d983b4f8744b040f28b046e25ecbb
lkusters <lienekekusters@gmail.com>
2016-10-07 10:07:09 +0200

    clean up
    remove some redundant files from repository

--- build/lib/contexttree/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct  6 10:12:30 2016
-
-@author: Lieneke Kusters
-"""
-
--- build/scripts-3.4/example.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct  6 12:15:53 2016
-
-@author: Lieneke Kusters
-"""
-
